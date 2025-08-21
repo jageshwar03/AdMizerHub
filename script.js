fd1296d39ae01a5809935be64a6cb88f701ab6da
jageshwar03 <jageshwarkumarsaw62@gmail.com>
2025-08-21 18:50:04 +0530

    privacy policy update

---
+++ script.js
@@ -0,0 +1,14 @@
+// get all question elements
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+    // toggle answer visibility
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+});
